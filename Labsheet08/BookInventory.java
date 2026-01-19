bfb9a61bf0c46282a9251806037b31ec53ed37be
ทนายราตรี Githubจริง <110712161+14thpeace@users.noreply.github.com>
2026-01-19 16:04:11 +0700

    2513110375

---
+++ Labsheet08/BookInventory.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+public class BookInventory {
+    public static void main(String[] args) {
+
+        Book[] books = new Book[3];
+
+        for (int i = 0; i < 3; i++) {
+            String title = JOptionPane.showInputDialog("Enter book title:");
+            int total = Integer.parseInt(
+                    JOptionPane.showInputDialog("Enter the total number:")
+            );
+            books[i] = new Book(title, total);
+        }
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(
+                    books[i].getTitle() + " has " + books[i].getTotalBook() +
+                            " books, can borrow " + books[i].getAvailableBook() + " books."
+            );
+        }
+    }
+}
---
+++ Labsheet08/BorrowBookTest.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+
+public class BorrowBookTest {
+    public static void main(String[] args) {
+
+        Book book = new Book("C# Programming", 5);
+
+        while (true) {
+            int confirm = JOptionPane.showConfirmDialog(
+                    null,
+                    "Do you want to borrow books?",
+                    "Borrow Book",
+                    JOptionPane.YES_NO_OPTION
+            );
+
+            if (confirm == JOptionPane.NO_OPTION) {
+                JOptionPane.showMessageDialog(null, "END PROGRAM");
+                break;
+            }
+
+            if (book.getAvailableBook() > 0) {
+                book.borrowBook();
+                System.out.println(
+                        "Borrowed 1 book, available " + book.getAvailableBook() + " books."
+                );
+            } else {
+                JOptionPane.showMessageDialog(null,
+                        "No books available to borrow...",
+                        "Warning Message",
+                        JOptionPane.WARNING_MESSAGE);
+            }
+        }
+    }
+}
