9e968a2b0610d9091b2ae148b6f972128f1561e9
ทนายราตรี Githubจริง <110712161+14thpeace@users.noreply.github.com>
2026-09-18 11:20:05 +0700

    //10

---
+++ LabSheet10/src/BSTApp1.java
@@ -0,0 +1,29 @@
+
+public class BSTApp1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BinarySearchTree tree = new BinarySearchTree();
+
+		tree.sampleTree1();
+		tree.printTree(tree.getRoot(), 0);
+		
+		System.out.println();
+		System.out.println("Minimum node is " + tree.findMinimum(tree.getRoot()));
+		System.out.println("Maximum node is " + tree.findMaximum(tree.getRoot()));
+		
+		int target = 40;
+		System.out.println("IS " + target + "int bst" tree.findSpecificData(target) );
+		
+		
+		int delNode = 60;
+		tree.searchDeleteNode(delNode);
+		System.out.println("Parent is " + tree.getParent().data);
+		System.out.println("Delete node is " + tree.getDeleteNode().data);
+		
+		System.out.println();
+		tree.delete(delNode);
+		tree.printTree(tree.getRoot(), 0);
+	}
+
+}
